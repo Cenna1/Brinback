173787b84cd21cbdacec94a07952e07a29bba262
Karen Dias <diasmkaren@gmail.com>
2024-01-12 23:14:57 -0300

    finalizacao do footer desktop e responsivo

--- controllers/footer.js
+++ controllers/footer.js
@@ -5,14 +5,42 @@ rodape.setAttribute("class", "rodape");
 
 body.appendChild(rodape);
 
-const paragrafo = "<p> Desenvolvido por: </p>" + 
+const paragrafo = "<p class = 'rodape_texto'> Desenvolvido por: </p>" + 
 "<div class= 'container_grid'> "+ 
 "<div>"+
 "<label> Açucena Oliveira</label>"+
-" <img src='./assets/img/icon _instagram.png' alt='icone intagram'>"+
-"<img src='./assets/img/icon _linkedin.png' alt='icone linkedin'>"+
-"<img src='./assets/img/icon _github.png' alt='icone github'>"+
+"<a href='#'><img src='./assets/img/icon _linkedin.png' alt='icone linkedin'></a>"+
+"<a href='#'><img src='./assets/img/icon _github.png' alt='icone github'></a>"+
 "</div>"+
+
+"<div>"+
+"<label>Josefina Pambula</label>"+
+"<a href='#'><img src='./assets/img/icon _linkedin.png' alt='icone linkedin'></a>"+
+"<a href='#'><img src='./assets/img/icon _github.png' alt='icone github'></a>"+
+"</div>"+
+
+"<div>"+
+"<label>Karen Dias</label>"+
+"<a href='#'><img src='./assets/img/icon _linkedin.png' alt='icone linkedin'></a>"+
+"<a href='#'><img src='./assets/img/icon _github.png' alt='icone github'></a>"+
+"</div>"+
+
+"<div>"+
+"<label>Malorie Ebang</label>"+
+"<a href='#'><img src='./assets/img/icon _linkedin.png' alt='icone linkedin'></a>"+
+"<a href='#'><img src='./assets/img/icon _github.png' alt='icone github'></a>"+
+"</div>"+
+
+"<div>"+
+"<label>Nubia Calderan</label>"+
+"<a href='#'><img src='./assets/img/icon _linkedin.png' alt='icone linkedin'></a>"+
+"<a href='#'><img src='./assets/img/icon _github.png' alt='icone github'></a>"+
+"</div>"+
+"</div>"+
+
+"<div>"+
+"<p class = 'rodape_info'>11ª turma Mulheres in Tech da Fly Educação e Cultura</p>"
 "</div>"
 
 rodape.innerHTML+= paragrafo;
+
